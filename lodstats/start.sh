1a3b43e2a24efeb6d8b13658964afca6d47a0101
Ivan Ermilov <earthquakesan@gmail.com>
2014-09-19 13:26:16 +0200

    decoupled rabbitmq

--- lodstats/start.sh
+++ lodstats/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sed -i s/REPLACE_WITH_THE_HOST/${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/g /lodstats_www/production.ini
+sed -i s/rabbitmqHost\ =\ \"__RABBITMQHOST__\"/rabbitmqHost=\"${RABBITMQ_PORT_5672_TCP_ADDR}\"/g /csv2rdf-wiki/csv2rdf/messaging/__init__.py
 
 /usr/bin/supervisord
---
+++ lodstats/start_processing.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+paster --plugin=rdfstats process_all_datasets
