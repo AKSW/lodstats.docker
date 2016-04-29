ab8284f4e84c49d26f64825b0646a8e585c3f117
Ivan Ermilov <IvanErmilov@akswnc8.informatik.uni-leipzig.de>
2016-04-29 19:52:23 +0200

    changed docker-compose to version one to use nginx proxy

--- web/start.sh
+++ web/start.sh
@@ -2,5 +2,6 @@
 
 sed -i s/REPLACE_WITH_THE_HOST/${LODSTATS_DB}:5432/g /lodstats_www/production.ini
 sed -i s/rabbitmqHost\ =\ \"__RABBITMQHOST__\"/rabbitmqHost=\"${RABBITMQ}\"/g /csv2rdf-wiki/csv2rdf/messaging/__init__.py
+sed -i s/port\ =\ 5000/port=80/g /lodstats_www/production.ini
 
 /usr/bin/supervisord
