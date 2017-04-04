64aeecd65043079284e125b3b75d33fd25a331d1
Konrad Abicht <k.abicht@googlemail.com>
2017-04-04 09:26:27 +0200

    small tweaks in web/start_processing.sh

--- web/start_processing.sh
+++ web/start_processing.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd /lodstats_www
+echo "STARTED CRON JOB: process_all_datasets" >> /var/log/supervisor/supervisord.log
 
-echo "STARTED CRON JOB" >> /var/log/supervisor/supervisord.log
-paster --plugin=rdfstats process_all_datasets
+cd /lodstats_www && paster --plugin=rdfstats process_all_datasets
